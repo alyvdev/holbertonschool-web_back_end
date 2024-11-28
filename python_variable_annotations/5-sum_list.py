e7c6eea83bad86ee9df30392335179dbc2c61bdb
Ali Aliyev <alyvofficial@gmail.com>
2024-11-28 14:06:25 +0400

    Update docstring in sum_list function for improved readability

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -7,6 +7,7 @@ from typing import List
 
 def sum_list(input_list: List[float]) -> float:
     """
-    This function takes a list of numbers as input and returns the sum of the numbers.
+    This function takes a list of numbers as input and
+    returns the sum of the numbers.
     """
     return sum(input_list)
