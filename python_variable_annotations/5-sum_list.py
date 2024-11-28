01ff11cd9cc3b0d7aeb82567e3a22e2180916dfa
Ali Aliyev <alyvofficial@gmail.com>
2024-11-28 14:05:50 +0400

    Refactor sum_list function to use List type annotation for input parameter

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 """
 This module demonstrates the use of type annotations in Python.
 """
+from typing import List
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     This function takes a list of numbers as input and returns the sum of the numbers.
     """
