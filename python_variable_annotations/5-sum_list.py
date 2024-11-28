5c2ee264b80b374e54a20ee566392093f7b8fd3c
Ali Aliyev <alyvofficial@gmail.com>
2024-11-28 14:02:10 +0400

    Add sum_list function with type annotations and main script for testing

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module demonstrates the use of type annotations in Python.
+"""
+
+
+def sum_list(input_list: list) -> float:
+    """
+    This function takes a list of numbers as input and returns the sum of the numbers.
+    """
+    return sum(input_list)
