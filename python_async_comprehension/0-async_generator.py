71b072d44aeac43196404fc66369522e0adc916c
Ali Aliyev <alyvofficial@gmail.com>
2024-12-01 22:23:41 +0400

    Add type hint for async_generator function to specify return type

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Async Generator"""
 import random
 import asyncio
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """Async Generator"""
     for _ in range(10):
         await asyncio.sleep(1)
