e450d33df3eb32281ab816033b6d8d5957f5de97
Ali Aliyev <alyvofficial@gmail.com>
2024-12-01 20:47:44 +0400

    Refactor wait_n function to use asyncio.create_task and asyncio.as_completed for improved task handling; update module docstring for clarity.

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
-"""Basic async syntax"""
+"""Python and Async coroutines"""
 import asyncio
-import random
 from typing import List
 
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_random(max_delay: int = 10) -> float:
-    """Wait for a random delay between 0 and max_delay"""
-
-    delay: float = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Wait for n random delays"""
-    tasks = [wait_random() for _ in range(n)]
-    return await asyncio.gather(*tasks)
+    """returns list of all delayed async tasks"""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delayed_tasks = [await task for task in asyncio.as_completed(tasks)]
+    return delayed_tasks
