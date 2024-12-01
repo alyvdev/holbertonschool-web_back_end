58b25acf523a2dc82a116bce53e683d855e00a38
Ali Aliyev <alyvofficial@gmail.com>
2024-12-01 22:02:22 +0400

    Add task_wait_n function to create multiple async tasks with wait_n

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""  alter it into a new function task_wait_n.
+The code is nearly identical to wait_n except
+task_wait_random is being called """
+
+from typing import List
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """returns delayed async task"""
+    delays = []
+    for _ in range(n):
+        delays.append(wait_n(n, max_delay))
+    return delays
