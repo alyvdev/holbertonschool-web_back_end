7c2fe802fabdab2ed23f54a70cee1b5bf88c361c
Ali Aliyev <alyvofficial@gmail.com>
2024-12-01 21:53:30 +0400

    Update measure_time function to use time.time for accuracy; improve docstring for clarity

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,9 +8,10 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """returns total_time / ni measure"""
-    start = time.perf_counter()
+    """returns total_time / n measure"""
+
+    start = time.time()
     asyncio.run(wait_n(n, max_delay))
-    end = time.perf_counter()
+    end = time.time()
     total_time = end - start
     return total_time / n
