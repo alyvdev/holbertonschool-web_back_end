84a7d16c2fe7575abd5fd992321cbadc924d2819
Ali Aliyev <alyvofficial@gmail.com>
2024-12-01 21:49:31 +0400

    Add main test files for async functions and measure runtime

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Measure the runtime"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Measures the runtime"""
+    start = time.perf_counter()
+    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
+    end = time.perf_counter()
+    total_time = end - start
+    return total_time / n
