d647796b5f948c14b418113a308a7a1960704932
Ali Aliyev <alyvofficial@gmail.com>
2024-12-07 13:30:55 +0400

    Remove unnecessary blank lines in LIFOCache class for improved readability

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -10,7 +10,6 @@ class LIFOCache(BaseCaching):
         ''' Initialize class instance. '''
         super().__init__()
         self.keys = []
-    
     def put(self, key, item):
         """ Add an item in the cache """
         if key and item is not None:
@@ -23,7 +22,6 @@ class LIFOCache(BaseCaching):
                 last = self.keys.pop(-2)
                 del self.cache_data[last]
                 print("DISCARD: {}".format(last))
-    
     def get(self, key):
         """ Get an item by key """
         if key is not None and key in self.cache_data:
