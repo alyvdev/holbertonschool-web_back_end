5bab0e6e6dd0aeb5861567bc66f4e005ddaa4c5c
Ali Aliyev <alyvofficial@gmail.com>
2024-12-07 12:17:57 +0400

    Refactor async_comprehension module to include asyncio import for improved functionality

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Async Generator"""
-async_generator = __import__('0-async_generator').async_generator
+import asyncio
 from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
