bd33f6ecfef73e04d7cf724db306c2fdbfb8831c
Ali Aliyev <alyvofficial@gmail.com>
2024-12-07 14:20:48 +0400

    Refactor LFUCache put and get methods to enhance item tracking and management

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -13,30 +13,29 @@ class LFUCache(BaseCaching):
         self.count = {}
 
     def put(self, key, item):
-        ''' Add key/value pair to cache data.
-            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
-            discard least frequently used entry to accommodate new entry. '''
-
-        if key is not None and item is not None:
-            if (len(self.keys) == BaseCaching.MAX_ITEMS and
-                    key not in self.keys):
-                discard = self.keys.pop(self.keys.index(self.findLFU()))
-                del self.cache_data[discard]
-                del self.uses[discard]
-                print('DISCARD: {:s}'.format(discard))
+        """ Add an item in the cache """
+        if key and item is not None:
             self.cache_data[key] = item
             if key not in self.keys:
                 self.keys.append(key)
-                self.uses[key] = 0
             else:
                 self.keys.append(self.keys.pop(self.keys.index(key)))
-                self.uses[key] += 1
+            if key in self.count:
+                self.count[key] += 1
+            else:
+                self.count[key] = 1
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discard = min(self.count, key=self.count.get)
+                self.count.pop(discard)
+                self.keys.pop(self.keys.index(discard))
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
-        ''' Return value stored in `key` key of cache.
-            If key is None or does not exist in cache, return None. '''
+        """ Return value stored in `key` key of cache.
+            If key is None or does not exist in cache, return None."""
         if key is not None and key in self.cache_data:
             self.keys.append(self.keys.pop(self.keys.index(key)))
-            self.uses[key] += 1
+            self.count[key] += 1
             return self.cache_data[key]
         return None
