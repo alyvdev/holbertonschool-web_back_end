2f19cd49d6d601ad60b4f45c4680ccc9337f6136
Ali Aliyev <alyvofficial@gmail.com>
2024-12-07 12:16:32 +0400

    Change return type of async_comprehension function to List[float] for type consistency

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Async Generator"""
 async_generator = __import__('0-async_generator').async_generator
-import asyncio
+from typing import List
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """Collect 10 random numbers using an async comprehensing"""
     return [i async for i in async_generator()]
