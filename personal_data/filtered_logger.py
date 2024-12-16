07e1fe62909097d3177a1b9977fce1ff647fc96f
Ali Aliyev <alyvofficial@gmail.com>
2024-12-16 21:10:50 +0400

    Add blank line for improved readability in filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ returns the log message obfuscated:
 from typing import List
 import re
 
+
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """returns the log message obfuscated"""
