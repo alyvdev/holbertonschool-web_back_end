40c98099df5c659e43a6e048410485f8d87ae8d8
Ali Aliyev <alyvofficial@gmail.com>
2024-12-16 21:28:39 +0400

    Refactor filter_datum and RedactingFormatter for improved documentation and functionality

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,50 +1,57 @@
 #!/usr/bin/env python3
-"""Write a function called filter_datum that
-returns the log message obfuscated:
 """
+Module to redact sensitive data from logs.
+"""
+
 from typing import List
 import re
 import logging
 
+# Define PII_FIELDS constant
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """ Constructor method
-            """
+        """ Constructor method """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ Redacting Formatter method
-            """
-        return filter_datum(self.fields, self.REDACTION,
-                            super().format(record), self.SEPARATOR)
+        """ Redacting Formatter method """
+        original_message = super().format(record)
+        return filter_datum(self.fields,
+                            self.REDACTION, original_message, self.SEPARATOR)
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """returns the log message obfuscated"""
-    for field in fields:
-        message = re.sub(rf'{field}=.*?{separator}',
-                         f'{field}={redaction}{separator}', message)
-    return message
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Obfuscates fields in a log message.
+    """
+    pattern = f"({'|'.join(fields)})=.*?{separator}"
+    return re.sub(pattern, lambda m: 
+                  f"{m.group(1)}={redaction}{separator}", message)
+
 
 def get_logger() -> logging.Logger:
-    """returns a logging.Logger object"""
+    """
+    Configures and returns a logger for user data.
+    """
     logger = logging.getLogger('user_data')
     logger.setLevel(logging.INFO)
     logger.propagate = False
+
+    # Create a handler with RedactingFormatter
     handler = logging.StreamHandler()
-    handler.setFormatter(RedactingFormatter
-                         (list(("name", "email", "phone"))))
-    PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-    handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+
+    # Add the handler to the logger
     logger.addHandler(handler)
     return logger
