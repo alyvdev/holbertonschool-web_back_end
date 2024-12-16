4c157a64d020c792471ad356c1a6643ec47e0412
Ali Aliyev <alyvofficial@gmail.com>
2024-12-16 21:14:45 +0400

    Implement RedactingFormatter class to enhance logging with sensitive data redaction

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,12 +4,30 @@ returns the log message obfuscated:
 """
 from typing import List
 import re
+import logging
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """returns the log message obfuscated"""
-    for field in fields:
-        message = re.sub(rf'{field}=.*?{separator}',
-                         f'{field}={redaction}{separator}', message)
-    return message
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+    
+    
+    def filter_datum(fields: List[str], redaction: str, message: str,
+                     separator: str) -> str:
+        """returns the log message obfuscated"""
+        for field in fields:
+            message = re.sub(rf'{field}=.*?{separator}',
+                             f'{field}={redaction}{separator}', message)
+        return message
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
