771b3e5479637c7b190d48945494f381bd0a6853
Ali Aliyev <alyvofficial@gmail.com>
2024-12-16 21:10:22 +0400

    Refactor filter_datum function for improved obfuscation logic and documentation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,32 +1,14 @@
 #!/usr/bin/env python3
-"""Filter log messages based on severity level."""
-
+"""Write a function called filter_datum that
+returns the log message obfuscated:
+"""
 from typing import List
-from re import sub
+import re
 
-
-def filter_datum(fields: List, redaction: str,
-                 message: str, separator: str) -> str:
-    """
-        Filter and obfuscated the string
-
-        Args:
-            fields: a list of strings representing all fields to obfuscate
-                    ["password", "date_of_birth"]
-            redaction: a string representing by what the
-                       field will be obfuscated
-                       "XXXXX"
-            message: a string representing the log line
-                    ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;"]
-                    ["name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
-            separator: a string representing by which character is
-                    separating all fields in the log line (message)
-                    ";"
-        Return:
-            String with string ofuscated
-    """
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """returns the log message obfuscated"""
     for field in fields:
-        message = sub(f'{field}=.+?{separator}',
-                      f'{field}={redaction}{separator}', message)
-
+        message = re.sub(rf'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
     return message
