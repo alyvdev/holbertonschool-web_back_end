d8f4bf9f43ce8ec1796c6e1bc45d0e1cf6da5209
Ali Aliyev <alyvofficial@gmail.com>
2024-12-23 17:44:35 +0400

    Refactor Cache class to rename set method to store and enhance documentation

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -7,12 +7,23 @@ import redis
 import uuid
 from typing import Union
 
+
 class Cache:
     def __init__(self):
+        """Initialize the Redis client and flush the database."""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def set(self, data: Union[str, bytes, int, float]) -> str:
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store the given data in Redis using a random key.
+
+        Args:
+            data (Union[str, bytes, int, float]): The data to store.
+
+        Returns:
+            str: The generated random key used to store the data.
+        """
+        key = str(uuid.uuid4())  # Generate a random UUID key as a string
+        self._redis.set(key, data)  # Store the data in Redis with the key
         return key
