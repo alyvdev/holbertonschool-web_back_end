ba4b181258dd6254d151aa75677dd38404b1d335
Ali Aliyev <alyvofficial@gmail.com>
2025-01-09 00:49:58 +0400

    Fix docstrings formatting in BasicAuth class for consistency

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,10 +6,14 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """ BasicAuth class that inherits from Auth """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """ Extracts the Base64 part of the Authorization header """
-        if authorization_header is None or not isinstance(authorization_header, str):
+    """BasicAuth class that inherits from Auth."""
+
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
+        """Extracts the Base64 part of the Authorization header."""
+        if (authorization_header is None or
+            not isinstance(authorization_header, str)):
             return None
         if not authorization_header.startswith("Basic "):
             return None
