8769b7156db4ca3bf0244f5bd462ebfb3acc3171
Ali Aliyev <alyvofficial@gmail.com>
2025-01-08 23:58:12 +0400

    Refactor formatting and improve readability in app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -5,15 +5,12 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
+from flask_cors import CORS
 from api.v1.auth.auth import Auth
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 
 auth = None
 auth_type = getenv("AUTH_TYPE")
@@ -21,15 +18,18 @@ auth_type = getenv("AUTH_TYPE")
 if auth_type == "auth":
     auth = Auth()
 
-
 @app.before_request
 def before_request():
     """ Before request handler
     """
     if auth is None:
-         return
+        return
 
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
     if not auth.require_auth(request.path, excluded_paths):
         return
 
@@ -39,13 +39,11 @@ def before_request():
     if auth.current_user(request) is None:
         abort(403)
 
-
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
-
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
@@ -53,13 +51,11 @@ def unauthorized(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
-
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
