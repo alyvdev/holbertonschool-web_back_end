6e9f2267d0e4f22a0cc50edc999e7081e55ded23
Ali Aliyev <alyvofficial@gmail.com>
2025-02-06 17:39:32 +0400

    .

---
+++ Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Session authentication views
+"""
+from os import getenv
+from flask import jsonify, request
+from api.v1.views import app_views
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """POST /api/v1/auth_session/login
+    Return:
+      - User object JSON represented
+    """
+    email = request.form.get('email')
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+
+    password = request.form.get('password')
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    users = User.search({'email': email})
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = users[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    response.set_cookie(getenv('SESSION_NAME'), session_id)
+    return response
