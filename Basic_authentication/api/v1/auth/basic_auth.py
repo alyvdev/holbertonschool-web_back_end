0b7b29a632597d043ca4658dde3719913700f720
Ali Aliyev <alyvofficial@gmail.com>
2025-02-06 15:59:28 +0400

    Retrieves the User instance for a request

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -68,3 +68,23 @@ class BasicAuth(Auth):
                 return user
 
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the User instance for a request"""
+        auth_header = self.authorization_header(request)
+        if not auth_header:
+            return None
+
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        if not base64_header:
+            return None
+
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        if not decoded_header:
+            return None
+
+        email, password = self.extract_user_credentials(decoded_header)
+        if not email or not password:
+            return None
+
+        return self.user_object_from_credentials(email, password)
