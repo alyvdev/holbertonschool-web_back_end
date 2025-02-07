36b9fb4cfde038010b31fd0e015a37e3f0c81574
Ali Aliyev <alyvofficial@gmail.com>
2025-02-07 14:34:08 +0400

    .

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -29,3 +29,13 @@ class SessionAuth(Auth):
         session_cookie = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_cookie)
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """Deletes the user session / Cookie from the request"""
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
