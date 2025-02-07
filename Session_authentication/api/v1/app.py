4f0c2dbd150bd4e6651ba688de631eeb3f353257
Ali Aliyev <alyvofficial@gmail.com>
2025-02-07 15:28:09 +0400

    .

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import CORS
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -21,6 +22,8 @@ if auth_type == "basic_auth":
     auth = BasicAuth()
 elif auth_type == "session_auth":
     auth = SessionAuth()
+elif auth_type == "session_exp_auth":
+    auth = SessionExpAuth()
 elif auth_type == "auth":
     auth = Auth()
 
