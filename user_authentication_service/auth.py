7bfd7e88482a59af6f461fca000747cedf300d9f
Ali Aliyev <alyvofficial@gmail.com>
2025-02-23 16:19:47 +0400

    add password hashing function to user authentication service; improve docstring clarity

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+"""
+    Encrypt a string
+"""
+import bcrypt
+from db import DB
+from user import Base, User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from uuid import uuid4
+
+
+def _hash_password(password: str = '') -> str:
+    """
+        Hashed the password
+
+        Args:
+            password: string to hashed
+        Return:
+            hashed password
+    """
+    hashed = bcrypt.hashpw(password.encode('utf-8'),
+                           bcrypt.gensalt(prefix=b"2b"))
+
+    hash_str: str = str(hashed.decode())
+
+    return hash_str
