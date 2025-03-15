0e5e63cd84246a5aa323d633481a575796880317
Ali Aliyev <alyvofficial@gmail.com>
2025-03-15 18:35:02 +0400

    refactor: simplify Flask app structure and improve Babel initialization

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
-""" Route module for the API """
+"""Basic Flask app"""
 
-from os import getenv
 from flask import Flask, request, render_template
 from flask_babel import Babel
-
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config(object):
+class Config:
     """ Setup - Babel configuration """
     LANGUAGES = ['en', 'fr']
-    # these are the inherent defaults just btw
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-# set the above class object as the configuration for the app
 app.config.from_object('3-app.Config')
 
 
@@ -30,13 +26,14 @@ def index() -> str:
     return render_template('3-index.html')
 
 
-@babel.localeselector
 def get_locale() -> str:
     """ Determines best match for supported languages """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+babel = Babel()
+babel.init_app(app, locale_selector=get_locale)
+
+
 if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host="0.0.0.0", port="5000", debug=True)
