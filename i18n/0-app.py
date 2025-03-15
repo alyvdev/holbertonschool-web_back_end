00f8489f21904e99398e496d112253f6c4d8baf6
Ali Aliyev <alyvofficial@gmail.com>
2025-03-15 17:47:10 +0400

    refactor: update Flask app structure and routing for API with environment configuration

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
-"""Create a simple Flask app."""
+""" Route module for the API - Basic Flask app """
 
+from os import getenv
 from flask import Flask, render_template
 
+app = Flask(__name__, static_url_path='')
 
-app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    """Return homepage."""
-    return render_template('index.html')
+    """ GET /
+        Return: 0-index.html
+    """
+    return render_template('0-index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
