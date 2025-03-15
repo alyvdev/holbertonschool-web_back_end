6826b3d631d50d88307e1282491fc49891ece68b
Ali Aliyev <alyvofficial@gmail.com>
2025-03-15 18:33:51 +0400

    refactor: update Flask app routing and configuration for API integration

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""Basic Flask app"""
+""" Route module for the API """
 
+from os import getenv
 from flask import Flask, request, render_template
 from flask_babel import Babel
+
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -11,30 +13,30 @@ babel = Babel(app)
 class Config(object):
     """ Setup - Babel configuration """
     LANGUAGES = ['en', 'fr']
+    # these are the inherent defaults just btw
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# set the above class object as the configuration for the app
 app.config.from_object('3-app.Config')
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     """ GET /
-    Return: 2-index.html
+    Return: 3-index.html
     """
     return render_template('3-index.html')
 
 
+@babel.localeselector
 def get_locale() -> str:
     """ Determines best match for supported languages """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-# Initialize Babel with the application
-babel = Babel()
-babel.init_app(app, locale_selector=get_locale)
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
