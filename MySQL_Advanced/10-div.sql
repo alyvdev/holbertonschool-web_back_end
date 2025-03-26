97be68413a57cd7aec84b16ed0d547170164691d
alyvdev <alyvofficial@gmail.com>
2025-03-27 03:21:59 +0400

    refactor: change DELIMITER syntax in MySQL function
    The DELIMITER statement was updated from '|' to ';' to align with standard SQL syntax and improve readability. This change ensures consistency and avoids potential issues with SQL parsing.

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,7 +1,7 @@
 -- Task 10: Safe divide - creates a function SafeDiv that divides
 -- (and returns) the first by the second number
 -- or returns 0 if the second number is equal to 0
-DELIMITER |
+DELIMITER;
 DROP FUNCTION IF EXISTS SafeDiv;
 CREATE FUNCTION SafeDiv (a INT, b INT)
 RETURNS FLOAT
