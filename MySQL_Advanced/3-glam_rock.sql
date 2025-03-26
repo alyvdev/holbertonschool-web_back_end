269abef15033616b225fcdf4ad92c38d14ed5376
alyvdev <alyvofficial@gmail.com>
2025-03-27 02:24:09 +0400

    refactor: improve query readability and accuracy in glam_rock.sql
    Refactor the SQL query to use `FIND_IN_SET` for more accurate style matching and improve readability by using backticks for column names. Also, simplify the lifespan calculation by removing redundant `IFNULL` checks.

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
--- Search bands with style Glam rock
--- Ranked by their longevity
-SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
-FROM metal_bands 
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+-- Task 3: Old school bandlists - all bands with Glam as their main style
+-- ranked by their longevity
+SELECT DISTINCT `band_name`,
+IFNULL(`split`, 2020) - `formed` as `lifespan`
+FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
+ORDER BY `lifespan` DESC;
