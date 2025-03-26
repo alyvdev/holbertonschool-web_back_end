3265816bc149d57f2084666228de7e4bea534c0b
alyvdev <alyvofficial@gmail.com>
2025-03-27 02:19:58 +0400

    refactor: update SQL query to rank Glam rock bands by longevity
    The SQL query was modified to rank bands by their lifespan in descending order. Additionally, the current year in the lifespan calculation was updated from 2020 to 2022 to reflect the most recent data.

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Search bands with style Glam rock
--- Durantion current
-
-SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
+-- Ranked by their longevity
+SELECT band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan 
 FROM metal_bands 
-WHERE style LIKE '%Glam rock%';
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
