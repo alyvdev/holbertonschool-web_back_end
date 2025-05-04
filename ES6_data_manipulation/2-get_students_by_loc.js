61349b176f972ab432d65397c8d4bb32ca02f352
alyvdev <alyvofficial@gmail.com>
2025-05-05 01:56:14 +0400

    refactor: convert getStudentsByLocation to arrow function and improve readability

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,9 @@
-import getListStudents from './0-get_list_students';
-function getStudentsByLocation(students, city) {
-  return students.filter((student) => student.location === city);
-}
+const getStudentsByLocation = (students, city) => {
+  const studentsLocation = students.filter(
+    (student) => student.location === city
+  );
+
+  return studentsLocation;
+};
 
 export default getStudentsByLocation;
