dbbb186daa5dc5b7813b085fff156a559de81a61
alyvdev <alyvofficial@gmail.com>
2025-05-05 02:03:45 +0400

    refactor: convert createInt8TypedArray to arrow function and improve implementation

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,16 @@
-export default function createInt8TypedArray(length, position, value) {
-  if (position >= length) {
+// Typed Array 8 Int
+
+const createInt8TypedArray = (length, position, value) => {
+  const buffer = new ArrayBuffer(length);
+  const data = new DataView(buffer);
+
+  if (position > length - 1) {
     throw new Error('Position outside range');
   }
-  const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
-  int8Array[position] = value;
 
-  return buffer;
-}
+  data.setInt8(position, value);
+
+  return data;
+};
+
+export default createInt8TypedArray;
