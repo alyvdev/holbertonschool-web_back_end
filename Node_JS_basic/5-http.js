c8b6abd32e9d5c0c7426398287ea6a120bab01ad
alyvdev <alyvofficial@gmail.com>
2025-05-05 02:38:15 +0400

    feat: implement HTTP server to handle requests and respond with student data

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer(async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const data = await countStudents(process.argv[2]);
+      res.end(data);
+    } catch (error) {
+      res.end(error.message);
+    }
+  } else {
+    res.statusCode = 404;
+    res.end('Not found');
+  }
+});
+app.listen(1245);
+
+module.exports = app;
