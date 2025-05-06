281e06fbcf59915fa2c7747efc3afb4cb84fe745
alyvdev <alyvofficial@gmail.com>
2025-05-06 15:09:02 +0400

    feat: add initial Redis client setup and .gitignore file

---
+++ queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,13 @@
+import { createClient } from 'redis';
+
+// Create Redis client
+const client = createClient();
+
+// Handle connection events
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
